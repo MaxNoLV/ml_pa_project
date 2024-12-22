f2f7f1a27bf6f940412faf8c5d30381cdf45b1c2
Maksims Noskovs <maksims.noskovs@gmail.com>
2024-12-22 12:22:51 +0200

    Final commit

--- dry_bean_pca.py
+++ dry_bean_pca.py
@@ -1,13 +1,10 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.io import arff
-from scipy import stats
 from sklearn.model_selection import train_test_split
-from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer, StandardScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -21,54 +18,14 @@ arff_file = arff.loadarff(
 )
 df = pd.DataFrame(arff_file[0])
 
-df.head()
-df.info()
-(df == 0).sum()
 
 df.rename(columns={"AspectRation": "AspectRatio"}, inplace=True)
 
-df["Class"].value_counts()
-
-# I don't like the format of the values in the column Class.
-# Decided to transform these values into str format
-
 df["Class"] = df["Class"].str.decode("utf-8")
 
-df.describe()
-
-# First visaulization
-
-df.hist(bins=50, figsize=(12, 12))
-plt.show()
-
-# Density plot
-
-sns.set_style("whitegrid")
 
 labels = df["Class"]
 features = df.drop("Class", axis=1)
-
-fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(16, 14))
-fig.suptitle("Density Plots for Each Feature", fontsize=14)
-
-axes = axes.flatten()
-
-for i, feature in enumerate(features.columns):
-    sns.kdeplot(
-        data=df,
-        x=feature,
-        hue="Class",
-        fill=True,
-        ax=axes[i],
-        palette="Set2",
-        alpha=0.5,
-    )
-    axes[i].set_title(f"Density of {feature}")
-    axes[i].set_xlabel("")
-    axes[i].set_ylabel("")
-
-plt.tight_layout(rect=[0, 0, 1, 0.96])
-plt.show()
 
 # Split into training (80%) and test (20%) sets using Stratified Sampling
 
@@ -102,9 +59,6 @@ pca_train_features = pipeline.transform(train_features)
 pca_val_features = pipeline.transform(val_features)
 pca_test_features = pipeline.transform(test_features)
 
-type(train_labels)
-train_labels.shape
-train_labels[:5]
 
 # Train SVM, DT and KNN models
 
